b5446b6ebce8dc8a193d705eb642ea3fdf0340bd
Magnus Bjerke Vik <mbvett@gmail.com>
2015-09-28 20:48:05 +0200

    Read profile and gbd tag

--- app/app.js
+++ app/app.js
@@ -1,14 +1,96 @@
 (function(){
     var app = angular.module('iccmax-gamut', []);
     app.controller('AppController', ['$scope', function($scope) {
-        this.title = 'GAMUT';
+        var activeGamut = {
+            vertices: [],
+            faces: []
+        }
+
+        var readGamut = function(dv, index, size) {
+            var numPcsChannels = dv.getUint16(index + 8);
+            var numDeviceChannels = dv.getUint16(index + 10);
+            var numVertices = dv.getUint32(index + 12);
+            var numFaces = dv.getUint32(index + 16);
+
+            var gamut = {
+                vertices: [],
+                faces: [],
+                channels: numPcsChannels
+            };
+
+            var faceIndex = index + 20;
+            for (var i = 0; i < numFaces; ++i) {
+                var face = new Array(3);
+                face[0] = dv.getUint32(faceIndex);
+                face[1] = dv.getUint32(faceIndex + 4);
+                face[2] = dv.getUint32(faceIndex + 8);
+
+                gamut.faces.push(face);
+
+                faceIndex = faceIndex + 12;
+            }
+
+            var vertexIndex = faceIndex;
+            for (var i = 0; i < numFaces; ++i) {
+                var vertex = new Array(numPcsChannels);
+                for (var j = 0; j < numPcsChannels; ++i) {
+                    vertex[j] = dv.getFloat32(vertexIndex + i * 4);
+                }
+
+                gamut.vertices.push(vertex);
+
+                vertexIndex = vertexIndex + 4 * numPcsChannels;
+            }
+
+            return gamut;
+        }
 
         var readProfile = function(file) {
             fileReader = new FileReader();
             fileReader.onloadend = function() {
-                console.log("Read a file: " + fileReader.result);
+                var dv = new DataView(fileReader.result);
+
+                var version = dv.getUint8(8);
+                console.log("Profile version: " + version);
+
+                if (version < 5) {
+                    console.log("Version is required to be 5 or higher");
+                    return false;
+                }
+
+                var tagTableIndex = 128;
+                var tagCountIndex = tagTableIndex;
+
+                var tagCount = dv.getUint32(tagCountIndex);
+                console.log("Tag count: " + tagCount);
+
+                var tagIndex = tagCountIndex + 4;
+                for (var i = 0; i < tagCount; ++i) {
+                    var sigIndex = tagIndex;
+                    var signature = String.fromCharCode(
+                        dv.getUint8(sigIndex),
+                        dv.getUint8(sigIndex + 1),
+                        dv.getUint8(sigIndex + 2),
+                        dv.getUint8(sigIndex + 3));
+
+                    var offsetIndex = sigIndex + 4;
+                    var offset = dv.getUint32(offsetIndex);
+
+                    var sizeIndex = offsetIndex + 4;
+                    var size = dv.getUint32(sizeIndex);
+
+                    console.log("Signature: " + signature);
+                    console.log("Offset: " + offset);
+                    console.log("Size: " + size);
+
+                    if (signature == 'gbd ') {
+                        activeGamut = readGamut(dv, offset, size);
+                    }
+
+                    tagIndex = tagIndex + 12;
+                }
             }
-            fileReader.readAsBinaryString(file);
+            fileReader.readAsArrayBuffer(file);
         }
 
         var renderCanvas = document.getElementById("visualizer-canvas");
